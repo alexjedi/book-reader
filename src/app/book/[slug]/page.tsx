496651787c8804c5c163e43a09537def14a9a684
Alex Shevlyakov <pxl.alexjedi@gmail.com>
2024-06-22 20:35:06 +0100

    Add reader page

--- src/app/book/[slug]/page.tsx
+++ src/app/book/[slug]/page.tsx
@@ -5,6 +5,8 @@ import { booksList } from '@/data/books'
 import Link from 'next/link'
 import Markdown from 'react-markdown'
 import { useEffect, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft, Settings } from 'lucide-react'
 
 const markdown = '../books/yields_of_civilization.md'
 
@@ -25,15 +27,32 @@ export default function BookPage({ params }: { params: any }) {
   if (!book) return <div>Book not found</div>
 
   return (
-    <div>
-      <img src={`/img/${book.cover}`} alt={book.name} />
-      <h1>{book.name}</h1>
-      <p>{book.writer}</p>
-      <p>
-        <Markdown>{text}</Markdown>
-      </p>
-      <Link href="/book">Back to book list</Link>
-      <Pagination />
-    </div>
+    <main className="w-screen flex flex-col items-center p-8 lg:p-24">
+      <header className="w-full flex justify-between max-w-[65ch] py-8">
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/">
+            <ArrowLeft className="h-4 w-4" />
+          </Link>
+        </Button>
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/">
+            <Settings className="h-4 w-4" />
+          </Link>
+        </Button>
+      </header>
+      <article className="prose prose-base lg:prose-lg w-full prose-p:text-gray-900">
+        <img
+          src={`/img/${book.cover}`}
+          alt={book.name}
+          className="w-full max-w-40 h-auto p-1 border border-background-primary"
+        />
+        <h1>{book.name}</h1>
+        <h4>{book.writer}</h4>
+        <p>
+          <Markdown>{text}</Markdown>
+        </p>
+        <Pagination />
+      </article>
+    </main>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased w-screen',
+          fontSans.variable
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
